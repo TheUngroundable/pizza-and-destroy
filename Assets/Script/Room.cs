6487c669805ef7ec08bfdde3824b8cfbcd00327c
Leonardo Ballari <leonardo.ballari@aveva.com>
2021-01-31 15:51:51 +0100

    Random room colors

--- Assets/Script/Room.cs
+++ Assets/Script/Room.cs
@@ -11,7 +11,31 @@ public class Room : MonoBehaviour
     public Transform doorD;
     public GameObject[] roomTypes;
     public GameObject doors;
-   
+    public Color[] colorA; 
+    public Color[] colorB; 
+    public Material matA;
+    public Material matB;
+
+
+
+    void Start()
+    {
+        int rnd = Random.Range(0,colorA.Length);
+        matA.color = colorA[rnd];
+        matB.color = colorB[rnd];
+
+       
+        transform.GetChild(1).GetComponent<Renderer>().materials[0].color = colorA[rnd];
+        transform.GetChild(1).GetComponent<Renderer>().materials[1].color = colorB[rnd];
+        transform.GetChild(2).GetComponent<Renderer>().materials[0].color = colorA[rnd];
+        transform.GetChild(2).GetComponent<Renderer>().materials[1].color = colorB[rnd];
+        transform.GetChild(3).GetComponent<Renderer>().materials[0].color = colorA[rnd];
+        transform.GetChild(3).GetComponent<Renderer>().materials[1].color = colorB[rnd];
+        transform.GetChild(4).GetComponent<Renderer>().materials[0].color = colorA[rnd];
+        transform.GetChild(4).GetComponent<Renderer>().materials[1].color = colorB[rnd];
+        
+    }
+
    public void DeleteTriggers()
    {
        Destroy(transform.GetChild(6).gameObject);
